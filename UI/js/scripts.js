45f7868554359624dce6c6b7c6f2bdc2ec86ba04
Soko Paul <sopapaso73@gmail.com>
2018-09-09 16:26:33 +0300

    add scripts to and button to close and open view of forms

---
+++ UI/js/scripts.js
@@ -0,0 +1,22 @@
+document.getElementById('login_button').addEventListener('click', function() {
+    openForms();
+});
+
+document.getElementById('close').addEventListener('click', function() {
+    closeForms();
+});
+
+function openForms() {
+    // Opens view for forms
+    const mq = window.matchMedia("(max-width: 620px)");
+    if (mq.matches) {
+        document.getElementById("all-forms").style.width = "90vw";
+    } else {
+        document.getElementById("all-forms").style.width = "40vw";
+    }
+}
+
+function closeForms() {
+    // closes view for forms
+    document.getElementById("all-forms").style.width = "0";
+}
