fcde4240a40e089cfbbe372595bc8981fed2e99c
Soko Paul <sopapaso73@gmail.com>
2018-09-11 18:00:56 +0300

    edit scripts add descriptive comments

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -12,6 +12,7 @@ try {
 
 try {
     document.getElementById('create_account').addEventListener('click', function() {
+        // switch to login form
         document.getElementById("signup-form").style.display = "none";
         document.getElementById("login-form").style.display = "block";
     });
@@ -19,12 +20,14 @@ try {
 
 try {
     document.getElementById('create_account_form').addEventListener('click', function() {
+        // switch to signup form
         document.getElementById("login-form").style.display = "none";
         document.getElementById("signup-form").style.display = "block";
     });
 } catch (err) {}
 try {
     document.getElementById('fast-orders').addEventListener('click', function(e) {
+        // open add quantity dialog
 
         if (e.target && isInArray(e.target.classList, "order-button")) {
             var element = document.getElementById("dialog-view");
@@ -36,6 +39,7 @@ try {
 
 try {
     document.getElementById('add-final').onclick = function() {
+        // close add quantity dialog
         var element = document.getElementById("dialog-view");
         element.classList.toggle("show");
         element.classList.toggle("hide");
@@ -54,6 +58,7 @@ try {
 
 try {
     document.getElementById('show_my_orders').onclick = function() {
+        // shows all menu and hides selected orders when menu button is clicked
         document.getElementById("my_orders").classList.add("show_my_orders");
         document.getElementById("my_orders").classList.replace("hide_my_orders", "show_my_orders");
         document.getElementById("all-orders").classList.add("hide_my_orders");
@@ -63,6 +68,7 @@ try {
 } catch (err) {}
 try {
     document.getElementById('show_all_orders').onclick = function() {
+        // hides all menu and shows selected orders when orders button is clicked
         document.getElementById("my_orders").classList.add("hide_my_orders");
         document.getElementById("my_orders").classList.replace("show_my_orders", "hide_my_orders");
         document.getElementById("all-orders").classList.add("show_my_orders");
