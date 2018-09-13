7a7a17d498b8073bef6a8cdca32b596fd482f8b1
Soko Paul <sopapaso73@gmail.com>
2018-09-13 10:24:31 +0300

    add place_order function and its tests

--- app/api/models/orders_manage.py
+++ app/api/models/orders_manage.py
@@ -3,7 +3,7 @@ import datetime
 
 class Order:
     """creates order"""
-    
+
     def __init__(self, order_title, order_description, order_price, delivery_location):
         self.order_title = order_title
         self.order_description = order_description
@@ -11,8 +11,17 @@ class Order:
         self.delivery_location = delivery_location
         self.created_at = datetime.datetime.now().timestamp()
         self.order_status = "incomplete"
+        self.order_id = ''
 
 
 class CustomerOrders:
-    def __init__():
-        pass
+    def __init__(self):
+        self.orders_list = []
+
+    def place_order(self, new_order: Order):
+        if len(self.orders_list) >= 0:
+            new_order.order_id = len(self.orders_list)
+            print(new_order.order_id)
+        else:
+            new_order.order_id = 0
+        self.orders_list.append(new_order)
--- tests/test_orders_manage.py
+++ tests/test_orders_manage.py
@@ -1,11 +1,12 @@
 import unittest
-from app import Order
+from app import Order, CustomerOrders
 
 
 class TestOrders(unittest.TestCase):
     def setUp(self):
         self.order = Order("order_title", "order_description",
                            "order_price", "delivery_location")
+        self.orders_list = CustomerOrders()
 
     def teardown(self):
         pass
@@ -17,3 +18,18 @@ class TestOrders(unittest.TestCase):
         with self.assertRaises(TypeError):
             Order("order_title", "order_description",
                   "order_price")
+
+    def test_order_list_instance(self):
+        self.assertIsInstance(self.orders_list, CustomerOrders)
+
+    def test_place_single_order(self):
+        self.orders_list.place_order(self.order)
+        self.assertEqual(len(self.orders_list.orders_list), 1)
+        self.assertEqual(self.orders_list.orders_list[0].order_id, 0)
+
+    def test_place_multiple_order(self):
+        self.orders_list.place_order(self.order)
+        self.orders_list.place_order(self.order)
+        self.orders_list.place_order(self.order)
+        self.assertEqual(len(self.orders_list.orders_list), 3)
+        self.assertEqual(self.orders_list.orders_list[2].order_id, 2)
