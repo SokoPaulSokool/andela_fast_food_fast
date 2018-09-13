30bccf4141a0c2e7d692d8d19dacd15edbdbf87e
Soko Paul <sopapaso73@gmail.com>
2018-09-13 17:04:02 +0300

    adjust tests for get all orders endpoint to first post data

--- tests/test_endpoint_get_all_orders.py
+++ tests/test_endpoint_get_all_orders.py
@@ -3,8 +3,12 @@ from app import Order, CustomerOrders, app
 import json
 
 
-class TestApis(unittest.TestCase):
+class TestGetOrders(unittest.TestCase):
 
     def test_get_all_endpoint(self):
-        response = app.test_client().get('/api/v1/orders')
-        self.assertEqual(json.loads(response.data), [])
+        test_client = app.test_client()
+        test_client.post('/api/v1/orders', data=json.dumps(dict(
+            order_title="order_title", order_description="order_description",
+            order_price="order_price", delivery_location="delivery_location")), content_type='application/json')
+        response = test_client.get('/api/v1/orders')
+        self.assertEqual(len(json.loads(response.data)), 1)
