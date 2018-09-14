17165d67923a2d6702893876167a0ae13fb36903
Soko Paul <sopapaso73@gmail.com>
2018-09-14 13:07:44 +0300

    add endpoint to update order status [Finishes #160472162]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from app.api.v1.endpoint_get_all_orders import api_get_all_orders
 from app.api.v1.endpoint_place_order import api_place_order
 from app.api.v1.endpoint_get_specific_order import api_get_specific_order
+from app.api.v1.endpoint_update_order_status import api_update_order_status
 
 app = Flask(__name__)
 
 app.register_blueprint(api_get_specific_order)
 app.register_blueprint(api_get_all_orders)
 app.register_blueprint(api_place_order)
+app.register_blueprint(api_update_order_status)
