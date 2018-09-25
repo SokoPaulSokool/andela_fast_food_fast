e7c76708f7c225cf8640107657cdc3ed79bdd1ae
Soko Paul <sopapaso73@gmail.com>
2018-09-25 16:14:51 +0300

    add tests for get menu endpoint [Delivers #160756110]

---
+++ tests/test_endpoint_get_menu.py
@@ -0,0 +1,11 @@
+import unittest
+from app import Order, CustomerOrders, app
+import json
+
+
+class TestGetOrders(unittest.TestCase):
+
+    def test_get_all_endpoint(self):
+        test_client = app.test_client()
+        response = test_client.get('/api/v1/menu')
+        self.assertGreater(len(json.loads(response.data)), 1)
