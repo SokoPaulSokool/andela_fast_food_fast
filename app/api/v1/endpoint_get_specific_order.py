42ef2925167f890b9ad591ff0ff13801fee25050
Soko Paul <sopapaso73@gmail.com>
2018-10-03 02:03:28 +0300

    configure jwt on orders endpoints [Delivers #160886972] [Delivers #160887358] [Delivers #160885014]

--- app/api/v1/endpoint_get_specific_order.py
+++ app/api/v1/endpoint_get_specific_order.py
@@ -1,16 +1,29 @@
 from flask import Blueprint, request, jsonify, json
 from app.api.models import Order, CustomerOrders, MessageResponse
 from app.api.v1.customer_orders import customer_orders
+from flask_jwt_extended import (
+    create_access_token,
+    verify_fresh_jwt_in_request,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    get_raw_jwt)
 
 api_get_specific_order = Blueprint('get_specific_order', __name__)
 
 
 @api_get_specific_order.route('/api/v1/orders/<int:order_id>', methods=['GET'])
+@jwt_required
 def get_specific_order(order_id):
     """gets user order by id"""
-    if request.method == "GET":
-        the_order = customer_orders.get_order(order_id)
-        if the_order == "order does not exist":
-            return MessageResponse.send(the_order, 404)
-        else:
-            return jsonify(customer_orders.get_order(order_id).toJSON()), 200
+    current_user = get_jwt_identity()
+    if current_user["user_type"] == 'admin':
+        if request.method == "GET":
+            the_order = customer_orders.get_order(order_id)
+            if the_order == "order does not exist":
+                return MessageResponse.send(the_order, 404)
+            else:
+                return jsonify(customer_orders.get_order(order_id).toJSON()), 200
+    else:
+        return MessageResponse().send("You need to be an admin to access this route", 406)
--- app/api/v1/endpoint_update_order_status.py
+++ app/api/v1/endpoint_update_order_status.py
@@ -1,21 +1,34 @@
 from flask import Blueprint, request, jsonify, json
 from app.api.models import Order, CustomerOrders, MessageResponse
 from app.api.v1.customer_orders import customer_orders
+from flask_jwt_extended import (
+    create_access_token,
+    verify_fresh_jwt_in_request,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    get_raw_jwt)
 
 api_update_order_status = Blueprint('update_order_status', __name__)
 
 
 @api_update_order_status.route('/api/v1/orders/<int:order_id>', methods=['PUT'])
+@jwt_required
 def update_order_status(order_id):
     """updates order status"""
-    if request.method == "PUT":
-        data = request.get_json()
-        if not data.get('order_status'):
-            return MessageResponse.send("either order_status is not set or empty", 406)
+    current_user = get_jwt_identity()
+    if current_user["user_type"] == 'admin':
+        if request.method == "PUT":
+            data = request.get_json()
+            if not data.get('order_status'):
+                return MessageResponse.send("either order_status is not set or empty", 406)
 
-        order_status = data.get('order_status')
-        status = customer_orders.change_status(order_id, order_status)
-        if status == "order does not exist":
-            return MessageResponse.send(status, 404)
-        else:
-            return MessageResponse.send(status, 200)
+            order_status = data.get('order_status')
+            status = customer_orders.change_status(order_id, order_status)
+            if status == "order does not exist":
+                return MessageResponse.send(status, 404)
+            else:
+                return MessageResponse.send(status, 200)
+    else:
+        return MessageResponse().send("You need to be an admin to access this route", 406)
