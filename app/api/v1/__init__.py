1b9b4acb93054d75e3815ad11ff9f2ef6d8dd073
Soko Paul <sopapaso73@gmail.com>
2018-10-03 04:29:01 +0300

    add endpoint for getting user's order history [Finishes #160887818]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from app.api.v1.endpoint_get_all_orders import api_get_all_orders
+from app.api.v1.endpoint_get_all_orders import api_get_all_orders, api_get_all_users_orders
 from app.api.v1.endpoint_place_order import api_place_order
 from app.api.v1.endpoint_get_specific_order import api_get_specific_order
 from app.api.v1.endpoint_update_order_status import api_update_order_status
@@ -23,6 +23,7 @@ app.register_blueprint(api_get_menu)
 app.register_blueprint(api_add_menu)
 app.register_blueprint(api_signup)
 app.register_blueprint(api_login)
+app.register_blueprint(api_get_all_users_orders)
 
 app.config['SECRET_KEY'] = os.environ.get(
     'SECRET_KEY', 'sokool-1234567')
