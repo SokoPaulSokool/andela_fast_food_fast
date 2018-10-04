8519f8ded7bd9abf3ba0b12c755fbbea7859ec58
Soko Paul <sopapaso73@gmail.com>
2018-10-04 12:03:57 +0300

    adjust connection string

--- app/api/models/database/connection.py
+++ app/api/models/database/connection.py
@@ -12,7 +12,7 @@ def connect():
         user='wwsmvpyvegrmoa',
         password='aee73a8991e50b401fd088a52d2ffd267fc989ab89a1aa19a2e2c91bb67fad4e',
         host="ec2-23-23-80-20.compute-1.amazonaws.com",
-    #     port='5432', sslmode='require')
+        port='5432', sslmode='require')
 
     # heroku2 = psycopg2.connect(DATABASE_URL, sslmode='require')
     # conn = psycopg2.connect(
